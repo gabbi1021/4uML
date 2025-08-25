23696afc518fa07e35bf1ae18ac5755172ced6d3
gabbi1021 <gabbmeoww@gmail.com>
2025-08-25 14:03:27 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `FOR YOUU`.split('')
+const text = `mwaaaahhhh`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
