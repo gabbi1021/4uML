03975d7c631cd256a1afe5b28d053c88107550e9
gabbi1021 <gabbmeoww@gmail.com>
2025-08-25 13:52:40 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('TANGINA MO').split('')
+    const titles = ('i loveee yiuuu sosos muchhh').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
